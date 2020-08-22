24a36746c014b2c383a16195c250739cdeaf328f
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2020-08-22 18:42:36 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.clearScreen()
 basic.forever(function () {
-    basic.showString("" + (Sensor.tempC(AnalogPin.P1)))
+    basic.showString("" + (Sensor.lightLevel(AnalogPin.P2)))
     basic.pause(500)
 })
